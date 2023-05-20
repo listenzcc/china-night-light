7120aba7fe8e878ae2e00127c891bf8653e194ad
listenzcc <listenzcc@mail.bnu.edu.cn>
2023-05-20 10:39:00 +0800

    Commit readme

--- data/data.py
+++ data/data.py
@@ -19,6 +19,7 @@ Functions:
 
 # %% ---- 2023-05-19 ------------------------
 # Pending
+import io
 import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
@@ -63,27 +64,39 @@ mat.shape
 
 # %%
 
-
-def imshow(arr, ax=None, show_flag=True):
-
-    if ax is None:
-        fig, ax = plt.subplots(1, 1)
-
-    im = ax.imshow(arr)
-    plt.colorbar(im)
-
-    fig.tight_layout()
-
-    if show_flag:
-        plt.show()
-
-
 std = np.std(mat, axis=0)
 mean = np.mean(mat, axis=0)
 
-imshow(std)
-imshow(mean)
+# %%
 
+
+def imshow(arr, ax=None, title='title'):
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    else:
+        fig = None
+
+    im = ax.imshow(arr)
+    ax.set_title(title)
+    ax.set_axis_off()
+    plt.colorbar(im)
+
+    return fig, im
+
+
+fig, axes = plt.subplots(2, 1, figsize=(6, 8), dpi=120)
+
+_, im = imshow(std, ax=axes[0], title='Std')
+_, im = imshow(mean, ax=axes[1], title='Mean')
+
+fig.tight_layout()
+plt.show()
+print(mat.shape, mean.shape, std.shape)
+
+# %%
+
+
+# %%
 
 # %%
 box = np.array(obj.tif_bBox)
@@ -91,7 +104,7 @@ print(box)
 converted = box / 1000 * 0.0174532925199433
 converted[:, 0] += 105
 converted[:, 1] /= 2
-converted
+print(converted)
 
 
 # %% ---- 2023-05-19 ------------------------
@@ -99,13 +112,13 @@ converted
 
 # %% ---- 2023-05-19 ------------------------
 # Pending
-m = mat.ravel()
-m = m[m > 0]
-print(m.shape)
-plt.hist(m)
+# m = mat.ravel()
+# m = m[m > 0]
+# print(m.shape)
+# plt.hist(m)
 
-# %%
-px.imshow(std).show()
-px.imshow(mean).show()
+# # %%
+# px.imshow(std).show()
+# px.imshow(mean).show()
 
 # %%
